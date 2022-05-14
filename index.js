762e326c3c9e30f07c26da32ba37c37714b78581
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-05-15 00:39:09 +0300

    This coomit contains the quiz page, it is made of questions rapt in a paragraph and input,radio,with lables holding the choices.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+  //function results(){
+  //  var score=0;
+  //  if (document.getElementById('thread').checked)
+  //  {
+  //  score++
+  //  }
+  //  if (document.getElementById('double_stroke').checked)
+  //  {
+  //   score++
+  //  }
+  //  if (document.getElementById('infinite').checked)
+  //  {
+  //    score++
+  //  }
+  //  if (document.getElementById('yes').checked)
+  //  {
+  //  score++
+  //  }
+  //  document.write('Your score is:'+score)
+  //}
+  //
